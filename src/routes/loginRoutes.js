a4dc4c013180c04576541055ac885df354c87680
GautamAbraham <gautamabraham147@gmail.com>
2021-05-17 15:03:02 +0530

    Check condition for login and signup

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -9,6 +9,25 @@ function router(nav) {
 			title: "Login",
 		});
 	});
+
+	loginRouter.post("/signin", function (req, res) {
+		userData.findOne(
+			{ email: req.body.email, password: req.body.password },
+			function (err, user) {
+				if (err || !user) {
+					// "if error or no user"
+					console.log("invalid Login cred");
+					res.redirect("/login");
+					// res.send("User not found.");
+				} else {
+					console.log("purrfect");
+					res.redirect("/");
+					// res.send("logged in!");
+				}
+			}
+		);
+	});
+
 	loginRouter.get("/signup", function (req, res) {
 		res.render("signup", {
 			nav,
@@ -25,8 +44,31 @@ function router(nav) {
 		};
 
 		var user = userData(item);
-		user.save();
-		res.redirect("/login");
+		userData.findOne({ email: item.email }, function (err, found) {
+			if (err || found) {
+				// "if error or no user"
+				console.log("invalid Login cred");
+				res.send("User already registered with given email ID");
+
+				// res.redirect("/login");
+				// res.send("User not found.");
+			} else {
+				console.log("purrfect");
+				user
+					.save()
+					.then((user) => {
+						console.log(user);
+						res.redirect("/login");
+						// res.send("User added succesfully.");
+					})
+					.catch((err) => {
+						console.log(err);
+						res.status(400).send("Unable to create user.");
+					});
+				// res.redirect("/");
+				// res.send("logged in!");
+			}
+		});
 	});
 
 	return loginRouter;
