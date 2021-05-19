4df5bf207fa5db37284b83e20a5858b369406898
GautamAbraham <gautamabraham147@gmail.com>
2021-05-19 13:45:16 +0530

    removed console logs

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -2,34 +2,27 @@ const express = require("express");
 const loginRouter = express.Router();
 const userData = require("../model/userData");
 
-var session=require('express-session');
-var flush=require('connect-flash');
-
-
-
-
+var session = require("express-session");
+var flush = require("connect-flash");
 
 function router(nav) {
-
-	loginRouter.use(session({
-		secret:'secter',
-		cookie:{maxAge:60000},
-		resave:false,
-		saveUninitialized:false,
-	
-	}));
+	loginRouter.use(
+		session({
+			secret: "secter",
+			cookie: { maxAge: 60000 },
+			resave: false,
+			saveUninitialized: false,
+		})
+	);
 	loginRouter.use(flush());
-
 
 	loginRouter.get("/", function (req, res) {
 		res.render("login", {
 			nav,
 			title: "Login",
-			message: req.flash('message')
+			message: req.flash("message"),
 		});
 	});
-
-	
 
 	loginRouter.post("/signin", function (req, res) {
 		userData.findOne(
@@ -37,12 +30,15 @@ function router(nav) {
 			function (err, user) {
 				if (err || !user) {
 					// "if error or no user"
-					console.log("invalid Login cred");
-					req.flash('message', 'Invalid Login credentials, Check email and password again');
+					// console.log("invalid Login cred");
+					req.flash(
+						"message",
+						"Invalid Login credentials, Check email and password again"
+					);
 					res.redirect("/login");
 					// res.send("User not found.");
 				} else {
-					console.log("purrfect");
+					// console.log("purrfect");
 					res.redirect("/");
 					// res.send("logged in!");
 				}
@@ -54,7 +50,7 @@ function router(nav) {
 		res.render("signup", {
 			nav,
 			title: "SignUp",
-			liss: req.flash('liss')
+			liss: req.flash("liss"),
 		});
 	});
 	loginRouter.post("/register", function (req, res) {
@@ -70,22 +66,25 @@ function router(nav) {
 		userData.findOne({ email: item.email }, function (err, found) {
 			if (err || found) {
 				// "if error or user already exist"
-				console.log("duplicate email");
+				// console.log("duplicate email");
 				// res.send("User already registered with given email ID");
-				req.flash('liss', 'Email address already taken, please try again with another one');
+				req.flash(
+					"liss",
+					"Email address already taken, please try again with another one"
+				);
 				res.redirect("/login/signup");
 				// res.send("User not found.");
 			} else {
-				console.log("purrfect");
+				// console.log("purrfect");
 				user
 					.save()
 					.then((user) => {
-						console.log(user);
+						// console.log(user);
 						res.redirect("/login");
 						// res.send("User added succesfully.");
 					})
 					.catch((err) => {
-						console.log(err);
+						// console.log(err);
 						res.status(400).send("Unable to create user.");
 					});
 				// res.redirect("/");
